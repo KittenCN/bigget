8574b66bf81f48f4dbc8b6f3b1197c414f06db25
KittenCN <owdely@vip.163.com>
2023-09-10 22:47:35 +0800

    update

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ def check_price(accountApi,markApi,orderApi,positionApi,symbol,marginCoin):
             ## record signal to file
             if total_score > 0:
                 centent = "Date:{}, Product:{}, Price:{:.2f}, Score:{:.2f}, Signal:{}, SignalValue:{}".format(current_datetime, symbol, current_price, total_score, current_signal, current_signal_value)
+                print(centent)
                 write_txt("./signal.txt", centent, rewrite=False)
             if crossMaxAvailable >= total_amount * 0.4 and current_signal == "buy":
                 use_amount = crossMaxAvailable * 0.7
