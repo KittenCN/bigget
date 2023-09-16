a2bf3e6a2741a634749a3f7e3cfe6b7cd44083c2
KittenCN <owdely@vip.163.com>
2023-09-16 15:43:19 +0800

    optimizer

--- main.py
+++ main.py
@@ -122,11 +122,13 @@ def check_price(accountApi,markApi,orderApi,positionApi,symbol,marginCoin):
                         StopLoss_rate = 1 - (presetStopLossPrice_rate[_i] / price_lever)
                         TakeProfit_rate = 1 + (presetTakeProfitPrice_rate[_i] / price_lever)
                         break
+                current_ticker_price = float(get_ticker(marketApi, symbol, print_info=False, market_id=market_id))
+                current_mark_price = float(get_mark(marketApi, symbol, print_info=False, market_id=market_id))
                 basecoin_size = use_amount / current_mark_price * price_lever
                 basecoin_size = math.floor(round(basecoin_size, 7) * 10**6) / 10**6
-                order_result = get_place_order(orderApi, symbol=symbol, marginCoin=marginCoin, size=basecoin_size, side='open_long', orderType='market', timeInForceValue='normal', clientOrderId=current_timestamp, print_info=False, presetStopLossPrice=round(current_mark_price*StopLoss_rate, 1), presetTakeProfitPrice=round(current_mark_price*TakeProfit_rate,1), market_id=market_id)
+                order_result = get_place_order(orderApi, symbol=symbol, marginCoin=marginCoin, size=basecoin_size, side='open_long', orderType='market', timeInForceValue='normal', clientOrderId=current_timestamp, print_info=False, presetStopLossPrice=round(current_mark_price*StopLoss_rate, 1), presetTakeProfitPrice=round(current_ticker_price*TakeProfit_rate,1), market_id=market_id)
                 order_status = order_result['msg'] if market_id == "bitget" else orderApi.get_all_orders(symbol=symbol, orderId=order_result['orderId'])[0]['status']
-                content = "Date:{}, Buy:{}, Side:{}, TickerPrice:{}, MarkPrice:{} size:{}, presetStopLossPrice:{}, presetTakeProfitPrice:{}, status:{}".format(current_datetime, symbol, 'open_long', current_ticker_price, current_mark_price, basecoin_size, round(current_mark_price*StopLoss_rate, 1), round(current_mark_price*TakeProfit_rate,1), order_status)
+                content = "Date:{}, Buy:{}, Side:{}, TickerPrice:{}, MarkPrice:{} size:{}, presetStopLossPrice:{}, presetTakeProfitPrice:{}, status:{}".format(current_datetime, symbol, 'open_long', current_ticker_price, current_mark_price, basecoin_size, round(current_mark_price*StopLoss_rate, 1), round(current_ticker_price*TakeProfit_rate,1), order_status)
                 if order_status.lower() == "success" or order_status == "FILLED":
                     print('\r' + '\033[42m' + content + '\033[0m')
                 else:
@@ -140,6 +142,8 @@ def check_price(accountApi,markApi,orderApi,positionApi,symbol,marginCoin):
                 print('\r' + '\033[31m' + content + '\033[0m')
             basecoin_size = 0
             basecoin_size, unrealizedPL = get_single_position(positionApi, symbol=symbol, marginCoin=marginCoin, print_info=False, market_id=market_id, positionSide="long")
+            current_ticker_price = float(get_ticker(marketApi, symbol, print_info=False, market_id=market_id))
+            current_mark_price = float(get_mark(marketApi, symbol, print_info=False, market_id=market_id))
             price_fee = basecoin_size * current_ticker_price * fee_rate
             unrealizedPL = unrealizedPL - price_fee
             # close long operation
@@ -176,11 +180,13 @@ def check_price(accountApi,markApi,orderApi,positionApi,symbol,marginCoin):
                         StopLoss_rate = 1 + (presetStopLossPrice_rate[_i] / price_lever)
                         TakeProfit_rate = 1 - (presetTakeProfitPrice_rate[_i] / price_lever)
                         break
+                current_ticker_price = float(get_ticker(marketApi, symbol, print_info=False, market_id=market_id))
+                current_mark_price = float(get_mark(marketApi, symbol, print_info=False, market_id=market_id))
                 basecoin_size = use_amount / current_mark_price * price_lever
                 basecoin_size = math.floor(round(basecoin_size, 7) * 10**6) / 10**6
-                order_result = get_place_order(orderApi, symbol=symbol, marginCoin=marginCoin, size=basecoin_size, side='open_short', orderType='market', timeInForceValue='normal', clientOrderId=current_timestamp, print_info=False, presetStopLossPrice=round(current_mark_price*StopLoss_rate,1), presetTakeProfitPrice=round(current_mark_price*TakeProfit_rate,1), market_id=market_id)
+                order_result = get_place_order(orderApi, symbol=symbol, marginCoin=marginCoin, size=basecoin_size, side='open_short', orderType='market', timeInForceValue='normal', clientOrderId=current_timestamp, print_info=False, presetStopLossPrice=round(current_mark_price*StopLoss_rate,1), presetTakeProfitPrice=round(current_ticker_price*TakeProfit_rate,1), market_id=market_id)
                 order_status = order_result['msg'] if market_id == "bitget" else orderApi.get_all_orders(symbol=symbol, orderId=order_result['orderId'])[0]['status']
-                content = "Date:{}, Sell:{}, Side:{}, TickerPrice:{}, MarkPrice:{}, size:{}, presetStopLossPrice:{}, presetTakeProfitPrice:{}, status:{}".format(current_datetime, symbol, 'open_short', current_ticker_price, current_mark_price, basecoin_size, round(current_mark_price*StopLoss_rate,1), round(current_mark_price*TakeProfit_rate,1), order_status)
+                content = "Date:{}, Sell:{}, Side:{}, TickerPrice:{}, MarkPrice:{}, size:{}, presetStopLossPrice:{}, presetTakeProfitPrice:{}, status:{}".format(current_datetime, symbol, 'open_short', current_ticker_price, current_mark_price, basecoin_size, round(current_mark_price*StopLoss_rate,1), round(current_ticker_price*TakeProfit_rate,1), order_status)
                 if order_status.lower() == "success" or order_status == "FILLED":
                     print('\r' + '\033[42m' + content + '\033[0m')
                 else:
@@ -194,6 +200,8 @@ def check_price(accountApi,markApi,orderApi,positionApi,symbol,marginCoin):
                 print('\r' + '\033[31m' + content + '\033[0m')
             basecoin_size = 0
             basecoin_size, unrealizedPL = get_single_position(positionApi, symbol=symbol, marginCoin=marginCoin, print_info=False, market_id=market_id, positionSide="short")
+            current_ticker_price = float(get_ticker(marketApi, symbol, print_info=False, market_id=market_id))
+            current_mark_price = float(get_mark(marketApi, symbol, print_info=False, market_id=market_id))
             price_fee = basecoin_size * current_ticker_price * fee_rate
             unrealizedPL = unrealizedPL - price_fee
             # close short operation
